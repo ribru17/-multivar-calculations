10fa38294a6d049fe5e48d6dce31344b4c90a479
Riley B <ribru17@hotmail.com>
2022-05-03 18:32:49 -0700

    helpful whitespace

--- lib/lineint.r
+++ lib/lineint.r
@@ -5,14 +5,17 @@ line_int <- function() {
     cat("Enter, separated by commas, r1, r2, r3 (where r(t) is a",
     "parametrization of a line), t lower bound, t upper bound, and",
     "f1, f2, f3 (where f(x,y,z) is a vector field)\n")
+
     # take and parse user input
     lineinput <- readline()
     split_line_input <- strsplit(lineinput, split = ",", fixed = TRUE)
+
     # construct r'(t)
     rprime <- list(stats::D(parse(text = split_line_input[[1]][1]), "t"),
     stats::D(parse(text = split_line_input[[1]][2]), "t"),
     stats::D(parse(text = split_line_input[[1]][3]), "t"))
     rprime <- lapply(rprime, deparse)
+
     # compute the components of f(r(t))
     str1 <- gsub("x", paste("(", split_line_input[[1]][1], ")"),
     split_line_input[[1]][6])
@@ -26,10 +29,12 @@ line_int <- function() {
     split_line_input[[1]][8])
     str3 <- gsub("y", paste("(", split_line_input[[1]][2], ")"), str3)
     str3 <- gsub("z", paste("(", split_line_input[[1]][3], ")"), str3)
+
     # construct f(r(t))
     fr <- list(str1, str2, str3)
-    cat("Line integral:\n")
+
     # evaluate the line integral
+    cat("Line integral:\n")
     print(eval(parse(text = paste("stats::integrate(function(t) {", fr[1], "*(",
     rprime[1], ")+", fr[2], "*(", rprime[2], ")+", fr[3], "*(",
     rprime[3], ")}, ", split_line_input[[1]][4], ",",
